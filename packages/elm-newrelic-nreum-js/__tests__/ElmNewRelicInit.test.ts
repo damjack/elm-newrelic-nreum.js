9cf7ac8eb55d8cc4314e0c91672f873206be784f
damjack <giacomello.damiano@gmail.com>
2023-02-09 08:09:29 +0100

    Fix tests

--- packages/elm-newrelic-nreum-js/__tests__/ElmNewRelicInit.test.ts
+++ packages/elm-newrelic-nreum-js/__tests__/ElmNewRelicInit.test.ts
@@ -1,5 +1,10 @@
 import { init } from '../src/index';
 
-test("Ok", () => {
-  true
+test("call init with different configuration", () => {
+  const init = {clientToken: 'XXX', applicationId: 'YYY'};
+  const testInit = jest.fn(args => init({clientToken: args.clientToken, applicationId: args.applicationId}));
+
+  testInit(init);
+
+  expect(testInit).toHaveBeenNthCalledWith(1, {clientToken: 'XXX', applicationId: 'YYY'});
 });
